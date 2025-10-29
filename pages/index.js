9a9825459ada08c9b63b8d33793f80c7e98ff25d
Evandro Xavier <xavi.evandros@gmail.com>
2025-10-29 18:07:26 -0300

    Adicionando fotos ao projeto

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
 function Home() {
-    return <h1>Essa é a página da Preta! Oi Preta!!!!</h1>;
-}
 
+    return (
+        <div>
+            <h1>Essa é a página da Preta! Oi Preta!!!!</h1>
+
+            {/* O caminho: Sair de 'pages', entrar em 'images', pegar 'preta.png' */}
+            <img src="/images/pretos.jpg"
+                alt="Preta!"
+                width="800"  // Define a largura para 300 pixels
+                height="1000" // Define a altura para 200 pixels
+            />
+
+        </div>
+    );
+}
 export default Home;
